84ee235a4a925e6535e51e5367236f19de5be27c
Tech-chandu1991 <chandududekula1991@gmail.com>
2026-03-15 23:47:22 +0530

    Terraform-vpc-module

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_internet_gateway" "main" {
 
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id       # it will associate(add) above created vpc to this public subnets
-  count = len(var.public_subnet_cidrs)  # ekkada roboshop project lo 2 public subnets kavali anduku count ni len function use chesi we are getting the 2 value
+  count = length(var.public_subnet_cidrs)  # ekkada roboshop project lo 2 public subnets kavali anduku count ni len function use chesi we are getting the 2 value
   cidr_block = var.public_subnet_cidrs[count.index] # ekkada oko subnet ki cidr variables lo list use chesi declare chesam akkada nunchi count.index ane function use chesi we are getting the cidr block for each subnet anna mata
   availability_zone = local.az_names[count.index]
   map_public_ip_on_launch = true
